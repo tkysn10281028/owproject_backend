9fef59e04040d017cf3ca0aeb24cdeaced97cba6
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2025-04-30 10:26:29 +0900

    User機能追加時の切り分けのため修正

--- src/main/java/com/oysterworld/portfolio/owproject_backend/security/SecurityConfig.java
+++ src/main/java/com/oysterworld/portfolio/owproject_backend/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
