a6348cb99a1410f541777274e35ab71d3d13b9ec
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2025-05-02 06:48:45 +0900

    DozerMapperの導入

---
+++ src/main/java/com/oysterworld/portfolio/owproject_backend/common/DozerMapperUtil.java
@@ -0,0 +1,20 @@
+package com.oysterworld.portfolio.owproject_backend.common;
+
+import com.github.dozermapper.core.DozerBeanMapper;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.List;
+
+public class DozerMapperUtil {
+    private static final Mapper mapper;
+    static {
+        mapper = DozerBeanMapperBuilder.create()
+                .withMappingFiles("dozerBeanMapping.xml")
+                .build();
+    }
+
+    public static <T> T map(Object source, Class<T> destinationClass) {
+        return mapper.map(source, destinationClass);
+    }
+}
